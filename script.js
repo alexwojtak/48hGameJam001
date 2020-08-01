fdf7b0c5cabffe342a33756d58ff397abe4a85a5
alex <alexwojtak@hotmail.com>
2020-08-01 16:57:06 +0100

    Add a win condition

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document.onkeydown = checkKey;
 
 let playerx = 0;
 let playery = 0;
+let doorx = 29;
+let doory = 8;
 
 //Stop arrow keys moving the screen around
 window.addEventListener("keydown", function(e) {
@@ -11,6 +13,14 @@ window.addEventListener("keydown", function(e) {
     }
 }, false);
 
+//Check win condition
+function checkWin() {
+	console.log("Position x is " + playerx + " and position y is " + playery);
+	if (playerx === doorx && playery === doory) {
+		console.log("win");
+		document.getElementById("winMessage").style.display=("inline");
+	}
+}
 
 //Keys to move the player
 function checkKey(e) {
@@ -22,31 +32,31 @@ function checkKey(e) {
 	    	playery--;
 		//Move player 24px (player dimension) in the desired direction
 		document.getElementById("playerChar").style.top=((24 * playery) + 'px');
+		checkWin();
 	}
-	console.log(playery);
     }
     else if (e.keyCode == '40') {
         if (playery<15){
         	playery++;
 		document.getElementById("playerChar").style.top=((24 * playery) + 'px');
+		checkWin();
         }
-        console.log(playery);
     }
     else if (e.keyCode == '37') {
 //Moving left
 	if (playerx>0){
 	    	playerx--;
 		document.getElementById("playerChar").style.left=((24 * playerx) + 'px');
+		checkWin();
 	}
-	console.log(playerx);
     }
     else if (e.keyCode == '39') {
 //Moving right
 	if (playerx<31){
 	    	playerx++;
 		document.getElementById("playerChar").style.left=((24 * playerx) + 'px');
+		checkWin();
 	}
-	console.log(playerx);
     }
 
 }
